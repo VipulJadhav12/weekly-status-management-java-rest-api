757033c00b12a390a2a8e90ff6aabe144cd882a9
VipulJadhav12 <vipulsjadhav.12@gmail.com>
2022-12-18 18:04:54 +0530

    Initial commit. Added raw Spring Boot v2.7.6 project.

---
+++ src/main/java/com/afourathon/weekly_status_management_rest_api/WeeklyStatusManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.afourathon.weekly_status_management_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyStatusManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyStatusManagementRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afourathon/weekly_status_management_rest_api/WeeklyStatusManagementRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afourathon.weekly_status_management_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyStatusManagementRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
