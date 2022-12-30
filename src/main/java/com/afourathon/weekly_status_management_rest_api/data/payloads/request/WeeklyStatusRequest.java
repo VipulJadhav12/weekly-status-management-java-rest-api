0602b910cf25d91c8bfe565b91bd1510a6a0fc23
VipulJadhav12 <vipulsjadhav.12@gmail.com>
2022-12-30 18:55:42 +0530

    Added no-args constructor for WeeklyStatus request entity. Updated env specific application.properties.

--- src/main/java/com/afourathon/weekly_status_management_rest_api/data/payloads/request/WeeklyStatusRequest.java
+++ src/main/java/com/afourathon/weekly_status_management_rest_api/data/payloads/request/WeeklyStatusRequest.java
@@ -1,13 +1,19 @@
 package com.afourathon.weekly_status_management_rest_api.data.payloads.request;
 
+import java.io.Serializable;
+
 import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
-public class WeeklyStatusRequest {
+public class WeeklyStatusRequest implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	@NotBlank(message = "Weekly Status cannot be null.")
 	private String status;
